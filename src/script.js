33849a32e6142d677ec27102e983f3af7de5a82b
kingdeepanshu <king.deepanshu24@gmail.com>
2024-06-28 00:55:34 +0530

    location api added and responsive

--- src/script.js
+++ src/script.js
@@ -1,6 +1,8 @@
-
 const city = document.querySelector("#search");
+const citym = document.querySelector("#searchm");
+
 const searchbut = document.querySelector("#searchbut")
+const searchbutm = document.querySelector("#searchbutm")
 
 const date = new Date();
 
@@ -10,10 +12,7 @@ let year = date.getFullYear();
 const months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
 document.querySelector("#date").innerHTML = `Today, ${day} ${months[month]} ${year}`;
 
-async function getWeather(city) {
-    const weatherRes = await fetch("https://api.openweathermap.org/data/2.5/weather?units=metric&q="+ city + "&appid=" + "5abc15c8e09813fc0d5ddd2d9568c993");
-    const weathers = await weatherRes.json();
-    console.log(weathers);
+function update(weathers){
     const temp = Math.round(weathers.main.temp)
     document.querySelector("#city").innerHTML = weathers.name;
     document.querySelector("#temp").innerHTML = temp +"°C";
@@ -21,6 +20,12 @@ async function getWeather(city) {
     document.querySelector("#wind").innerHTML = weathers.wind.speed +" km/h";
     document.querySelector("#humidity").innerHTML = weathers.main.humidity + "%";
     document.querySelector("#weather").innerHTML = weathers.weather[0].description;
+
+    document.querySelector("#clouds").innerHTML = weathers.clouds.all;
+    document.querySelector("#maxmintemp").innerHTML = `${Math.round(weathers.main.temp_max)}°C/${Math.round(weathers.main.temp_min)}°C`;
+    
+    document.querySelector("#sunrise").innerHTML = convertTimestamp(weathers.sys.sunrise);
+    document.querySelector("#sunset").innerHTML = convertTimestamp(weathers.sys.sunset);
 
     switch (weathers.weather[0].main) {
         case "Rain":
@@ -44,7 +49,38 @@ async function getWeather(city) {
     }
 }
 
+async function getWeather(city) {
+    const weatherRes = await fetch("https://api.openweathermap.org/data/2.5/weather?units=metric&q="+ city + "&appid=" + "5abc15c8e09813fc0d5ddd2d9568c993");
+    const weathers = await weatherRes.json();
+    console.log(weathers);
+    update(weathers);
+}
+
+async function inigetWeather(lat, lon) {
+    const weatherRes = await fetch(`https://api.openweathermap.org/data/2.5/weather?&units=metric&lat=${lat}&lon=${lon}&appid=5abc15c8e09813fc0d5ddd2d9568c993`);
+    const weathers = await weatherRes.json();
+    console.log(weathers);
+    update(weathers);
+}
+
 searchbut.addEventListener("click", () => {
     getWeather(city.value);
 })
+searchbutm.addEventListener("click", () => {
+    getWeather(citym.value);
+})
+
+function convertTimestamp(timestamp) {
+    const date = new Date(timestamp * 1000);
+    let hour = date.getHours();
+    const min = date.getMinutes();
+    const t = hour >= 12? 'PM' : 'AM';
+    hour = hour%12 == 0? 12 : hour%12;
+    return `${hour}:${min} ${t}`;    
+}
+
+navigator.geolocation.getCurrentPosition((position) => {
+    inigetWeather(position.coords.latitude, position.coords.longitude);
+  });
+
 
